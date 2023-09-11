e5c4d911671d05e702836f68b30623dafcb41994
Pedro Manfio Lill <pedro.manfiolill@gmail.com>
2023-09-11 16:18:33 -0300

    adicionando para teste

--- app.js
+++ app.js
@@ -2,3 +2,5 @@ console.log("Rodando o sistema de gerencimaneto de jogos!!!!")
 console.log("subindo o banco de dados")
 
 console.log("add alguma coisa")
+
+console.log("add mais um para voltar mais um pouco")
